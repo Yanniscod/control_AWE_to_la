afcc0c87c3bd3546c2b62d73158635705080a42c
Yanniscod <yannis.coderey@epfl.ch>
2025-04-28 16:12:18 +0200

    improved solving time to 4ms: switched to SQP_RTI, N=40, tolerance to 1e-4

--- drone_tether/tether_fo.py
+++ drone_tether/tether_fo.py
@@ -3,7 +3,8 @@ from tether_model import export_drone_tether_fo_model
 import numpy as np
 import scipy.linalg
 from utils import plot_drone_tet_fo_eval
-from casadi import SX, vertcat, Function, sqrt, fmax, sin, cos
+from casadi import SX, vertcat, Function, sqrt, fmax, sin, cos, pi
+import time
 
 def solve_ocp():
     ocp = AcadosOcp()
@@ -17,28 +18,28 @@ def solve_ocp():
     nu = model.u.rows()
     ny = nx + nu
     ny_e = nx
-    N = 50 # horizon
+    N = 40 # horizon
 
     # set dimensions
     ocp.solver_options.N_horizon = N
 
     # State cost
     Q = np.eye(nx)
-    Q[0,0] = 1.0     # x
-    Q[1,1] = 1.0     # y
+    Q[0,0] = 10.0     # x
+    Q[1,1] = 10.0     # y
     Q[2,2] = 10.0        # z
     Q[3,3] = 1.0     # phi
     Q[4,4] = 1.0     # theta
     Q[5,5] = 1.0     # psi
-    Q[6,6] = 0.0       # vwx
+    Q[6,6] = 1.0       # vwx
     Q[7,7] = 1.0        # vwy
     Q[8,8] = 1.0        # vwz
 
     # Input cost
     R = np.eye(nu)
-    R[0,0] = 0.0    # phi_cmd
-    R[1,1] = 0.0    # theta_cmd
-    R[2,2] = 0.0    # psi_cmd
+    R[0,0] = 1.0    # phi_cmd
+    R[1,1] = 1.0    # theta_cmd
+    R[2,2] = 1.0    # psi_cmd
     R[3,3] = 10.0    # thrust
 
     ocp.cost.W = scipy.linalg.block_diag(Q, R)
@@ -64,32 +65,33 @@ def solve_ocp():
     ocp.model.cost_y_expr_e = vertcat(ocp.model.x)
 
     # set constraints
-    ocp.constraints.lbu = np.array([-np.pi/4, -np.pi/4, -np.pi, 0.0])
-    ocp.constraints.ubu = np.array([np.pi/4, np.pi/4, np.pi, 1.0])
+    ocp.constraints.lbu = np.array([-pi/4, -pi/4, -pi, 0.0])
+    ocp.constraints.ubu = np.array([pi/4, pi/4, pi, 1.0])
     ocp.constraints.idxbu = np.arange(nu)
 
-    ocp.constraints.x0 = np.array([0.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-    x0 = ocp.constraints.x0
+    x0 = np.array([0.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+    ocp.constraints.x0 = x0
     u0 = np.array([0.0, 0.0, 0.0, 0.5])
-    ocp.constraints.lbx = np.array([-200.0, -200.0, 0.0, -np.pi/4, -np.pi/4,
-        -np.pi, -10.0, -10.0, -10.0])
-    ocp.constraints.ubx = np.array([+200.0, +200.0, +200.0, +np.pi/6, +np.pi/6,
-        +np.pi, +10.0, +10.0, +10.0])
+    ocp.constraints.lbx = np.array([-200.0, -200.0, 0.0, -pi/4, -pi/4,
+        -pi, -10.0, -10.0, -10.0])
+    ocp.constraints.ubx = np.array([+200.0, +200.0, +200.0, +pi/4, +pi/4,
+        +pi, +10.0, +10.0, +10.0])
     ocp.constraints.idxbx = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8])
 
-    f_expl_fun = Function("f_expl_fun", [model.x, model.u], [model.f_expl_expr])
-    print("f_expl at x0:", f_expl_fun(x0, u0))
+    # f_expl_fun = Function("f_expl_fun", [model.x, model.u], [model.f_expl_expr])
+    # print("f_expl at x0:", f_expl_fun(x0, u0))
 
-    cost_y_fun = Function("cost_y_fun", [model.x, model.u], [model.cost_y_expr])
-    print("cost_y_expr at x0:", cost_y_fun(x0, u0))
+    # cost_y_fun = Function("cost_y_fun", [model.x, model.u], [model.cost_y_expr])
+    # print("cost_y_expr at x0:", cost_y_fun(x0, u0))
 
     # set options
-    ocp.solver_options.print_level = 3
+    # ocp.solver_options.print_level = 3
     ocp.solver_options.qp_solver = 'FULL_CONDENSING_QPOASES' # FULL_CONDENSING_QPOASES
     ocp.solver_options.hessian_approx = 'GAUSS_NEWTON'
     ocp.solver_options.integrator_type = 'ERK'
-    ocp.solver_options.nlp_solver_type = 'SQP' # SQP_RTI, SQP
+    ocp.solver_options.nlp_solver_type = 'SQP_RTI' # SQP_RTI, SQP
     ocp.solver_options.tf = Tf
+    ocp.solver_options.tol = 1e-4
 
     ocp_solver = AcadosOcpSolver(ocp)
 
@@ -111,20 +113,27 @@ def solve_ocp():
         ocp_solver.constraints_set(0, 'lbx', x_init) # == data from sensor
         ocp_solver.constraints_set(0, 'ubx', x_init)
 
+        # set circular ref
+        yref[0] = 1.0*cos(theta_ref)
+        yref[1] = 1.0*sin(theta_ref)
+        yref[2] = 1.0
+        for i in range(N):
+            ocp_solver.set(i, "yref", yref)
+        ocp_solver.set(N, "yref", yref[:nx])
+
+        t0 = time.time()
         status = ocp_solver.solve()
+        t1 = time.time()
+        print(f"solve time: {(t1-t0)*1000} ms")
 
         if status != 0:
             ocp_solver.print_statistics() # encapsulates: stat = ocp_solver.get_stats("statistics")
             raise Exception(f'acados returned status {status}.')
 
-        yref[0] = 1.0*sin(theta_ref) # x_w
-        yref[1] = 1.0*cos(theta_ref) # y_w
-        yref[2] = 1.0
         # get solution
         for i in range(N):
             simX[i,:] = ocp_solver.get(i, "x")
             simU[i,:] = ocp_solver.get(i, "u")
-            # ocp_solver.set(i, "yref",yref)
         simX[N,:] = ocp_solver.get(N, "x")
 
         # shift, reuse prev solution as initial guess
@@ -142,7 +151,7 @@ def solve_ocp():
 
         # ocp_solver.print_statistics() # encapsulates: stat = ocp_solver.get_stats("statistics")
 
-    # plot_drone_tet_fo_eval(np.linspace(0, Tf, N+1), np.pi/4, simU, simX, latexify=True)
+    plot_drone_tet_fo_eval(np.linspace(0, Tf, N+1), pi/4, simU, simX, latexify=True)
 
 if __name__ == "__main__":
     solve_ocp()
